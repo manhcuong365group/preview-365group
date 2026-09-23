23158d9279462a741ecb1ca15fc301ba81340f44
manhcuong365group <contact@365group.vn>
2026-09-23 18:35:06 +0700

    content: remove the word zin from dropdown option

---
+++ scratch/check_runsearch_error.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let html = fs.readFileSync('auto365/bong-led/index.html', 'utf8');
+
+const s1 = html.indexOf('function runSearch()');
+const end = html.indexOf('}', html.indexOf('if (!socket || isNaN(voltage))')) + 10;
+console.log(html.substring(s1, end));
---
+++ scratch/fix_zin2.js
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+let html = fs.readFileSync('auto365/bong-led/index.html', 'utf8');
+
+html = html.replace('Đi phố, thay bóng Halogen zin', 'Đi phố, thay bóng Halogen');
+
+fs.writeFileSync('auto365/bong-led/index.html', '\ufeff' + html.replace(/^\ufeff/, ''), 'utf8');
+console.log("Removed zin.");
